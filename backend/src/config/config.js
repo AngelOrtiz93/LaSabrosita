e5c8c54b8c26e6b95d0d0d801bd025778c9de712
Laortiz937 <laortiz937@misena.edu.co>
2024-10-22 12:58:36 -0500

    variables postgres

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,11 +1,13 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql', 
+    username: process.env.DB_USER || 'restaurante_k32a_user',  // Cambia 'tu_usuario' si no usas variables de entorno
+    password: process.env.DB_PASSWORD || 'wIe8VYt3uXPHlm5Xz0dtsOjfA9mFXOLh',  // Cambia 'tu_contraseña' si no usas variables de entorno
+    database: process.env.DB_NAME || 'restaurante',  // Cambia 'nombre_base_datos' si no usas variables de entorno
+    host: process.env.DB_HOST || 'dpg-csbuanbv2p9s738pqou0-a',  // Cambia 'tu_host_render' si no usas variables de entorno
+    dialect: 'mysql'||'postgres',  // Dialecto para PostgreSQL
+    port: process.env.DB_PORT || 5432,  // Cambia el puerto si es diferente
   },
 };
+
