1a30d6335b75cbf9ff3834c7bdecfed7041b8b4d
Laortiz937 <laortiz937@misena.edu.co>
2024-10-24 14:09:44 -0500

    confirguracion postgres

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,14 +7,14 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,  // Cambia si no usas variables de entorno
     dialect: 'mysql',  // Especifica el dialecto correcto, en este caso 'mysql'
-    port: process.env.DB_PORT || 5432,  // Cambia si usas un puerto diferente para MySQL
+    port: process.env.DB_PORT || 3306,  // Cambia a 3306, que es el puerto por defecto de MySQL
   },
   production: {
-    username: process.env.DB_USER || 'restaurante_k32a_user',  // Cambia si no usas variables de entorno
-    password: process.env.DB_PASSWORD || 'wIe8VYt3uXPHlm5Xz0dtsOjfA9mFXOLh',  // Cambia si no usas variables de entorno
-    database: process.env.DB_NAME || 'restaurante',  // Cambia si no usas variables de entorno
-    host: process.env.DB_HOST || 'dpg-csbuanbv2p9s738pqou0-a',
+    username: 'admin',  // Usuario de la base de datos
+    password: 'AvQRYUDVNRJxqkPEyxs6bYHSkPi7HtSs',  // Contraseña de la base de datos
+    database: 'testdb_yyij',  // Nombre de la base de datos
+    host: 'dpg-csd9ifrqf0us73b21fog-a',  // Host de la base de datos
     dialect: 'postgres',  // Asegúrate de que esté configurado a 'postgres'
-    port: process.env.DB_PORT || 5432, 
+    port: 5432,  // Puerto por defecto de PostgreSQL
   },
 };
