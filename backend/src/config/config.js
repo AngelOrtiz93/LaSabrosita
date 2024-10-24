1c1f1b0f661745803171978189e4b1b4b4e4c3d2
Laortiz937 <laortiz937@misena.edu.co>
2024-10-24 14:18:20 -0500

    pruebas de conexion

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
+const env = process.env.NODE_ENV || 'development';  // Por defecto 'development'
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,  // Cambia si no usas variables de entorno
-    dialect: 'mysql',  // Especifica el dialecto correcto, en este caso 'mysql'
-    port: process.env.DB_PORT || 3306,  // Cambia a 3306, que es el puerto por defecto de MySQL
+    host: process.env.DB_HOST,
+    dialect: 'mysql',  // MySQL para desarrollo
+    port: process.env.DB_PORT || 3306,
   },
   production: {
-    username: 'admin',  // Usuario de la base de datos
-    password: 'AvQRYUDVNRJxqkPEyxs6bYHSkPi7HtSs',  // Contraseña de la base de datos
-    database: 'testdb_yyij',  // Nombre de la base de datos
-    host: 'dpg-csd9ifrqf0us73b21fog-a',  // Host de la base de datos
-    dialect: 'postgres',  // Asegúrate de que esté configurado a 'postgres'
-    port: 5432,  // Puerto por defecto de PostgreSQL
+    username: process.env.DB_USER,  // Obtiene el usuario desde el .env
+    password: process.env.DB_PASSWORD,  // Obtiene la contraseña desde el .env
+    database: process.env.DB_NAME,  // Obtiene el nombre de la base de datos desde el .env
+    host: process.env.DB_HOST,  // Obtiene el host desde el .env
+    dialect: 'postgres',  // PostgreSQL para producción
+    port: process.env.DB_PORT || 5432,  // Puerto por defecto para PostgreSQL
   },
-};
+}[env];  // Exporta la configuración correspondiente
